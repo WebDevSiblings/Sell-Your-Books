510ffa478e1123bcbe3e385913a4d455824c9bc4
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-17 23:09:42 +0300

    Add a pie chart from chart.js

---
+++ book-hub/src/components/Chart.tsx
@@ -0,0 +1,32 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Like", "Dislike"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [950000, 50000],
+      backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)"],
+      borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function Chart() {
+  return (
+    <div
+      style={{
+        height: "60vh",
+        position: "relative",
+        marginBottom: "1%",
+        padding: "1%",
+      }}
+    >
+      <Pie data={data} />
+    </div>
+  );
+}
--- book-hub/src/components/Welcome.tsx
+++ book-hub/src/components/Welcome.tsx
@@ -1,3 +1,4 @@
+import { Chart } from "./Chart";
 import IconText from "./IconText";
 import NavBar from "./NavBar";
 
@@ -6,6 +7,7 @@ const Welcome = () => {
     <>
       <NavBar />
       <IconText />
+      <Chart />
     </>
   );
 };
