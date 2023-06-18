106343b1a07588940d1b4144051590933a3ac64b
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-18 12:58:12 +0300

    Route the login and signup pages to the welcome page

--- book-hub/src/components/NavBar.tsx
+++ book-hub/src/components/NavBar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar bg-body-tertiary navbar-fixed-top ">
       <div className="colorControl container-fluid d-flex justify-content-between align-items-center ">
-        <h3 className="navbar-brand">Log in</h3>
-        <h3 className="navbar-brand">Sign up</h3>
+        <NavLink to="/login" className="navbar-brand">
+          Log in
+        </NavLink>
+        <NavLink to="/signup" className="navbar-brand">
+          Sign up
+        </NavLink>
       </div>
     </nav>
   );
