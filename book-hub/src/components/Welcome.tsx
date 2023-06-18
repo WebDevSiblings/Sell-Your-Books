1ff38071cad337a232284124c3ef0449f7764b72
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-18 14:22:55 +0300

    Fix NavBar and add image that routes to the Welcome page

--- book-hub/src/components/Welcome.tsx
+++ book-hub/src/components/Welcome.tsx
@@ -1,12 +1,10 @@
 import { Chart } from "./Chart";
-import IconText from "./IconText";
 import NavBar from "./NavBar";
 
 const Welcome = () => {
   return (
     <>
       <NavBar />
-      <IconText />
       <Chart />
     </>
   );
