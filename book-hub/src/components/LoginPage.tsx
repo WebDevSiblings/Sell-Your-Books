3bdea1a10a4d39178d44ee790015d13b19868357
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-20 12:45:42 +0300

    Add a small line below the NavBar

--- book-hub/src/components/LoginPage.tsx
+++ book-hub/src/components/LoginPage.tsx
@@ -1,10 +1,12 @@
 import Login from "./Login";
 import NavBar from "./NavBar";
+import WavyLine from "./WavyLine";
 
 const LoginPage = () => {
   return (
     <>
       <NavBar />
+      <WavyLine />
       <Login />
     </>
   );
--- book-hub/src/components/SignupPage.tsx
+++ book-hub/src/components/SignupPage.tsx
@@ -1,10 +1,12 @@
 import NavBar from "./NavBar";
 import Signup from "./Signup";
+import WavyLine from "./WavyLine";
 
 const LoginPage = () => {
   return (
     <>
       <NavBar />
+      <WavyLine />
       <Signup />
     </>
   );
