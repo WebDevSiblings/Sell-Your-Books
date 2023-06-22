61cd77a455ae7dfef1efc122286a10e52b583f74
Tia Beiruty <tiabeiruty@gmail.com>
2023-06-22 18:06:03 +0300

    Add book card

--- book-hub/src/components/Explore.tsx
+++ book-hub/src/components/Explore.tsx
@@ -1,21 +1,7 @@
-interface Props {
-  title: string;
-  subject: string | number;
-  grade: number | string;
-  price: number | string;
-  description: string;
-}
-
-const obj: Props = {
-  title: "Example Title",
-  subject: "Example Subject",
-  grade: 10,
-  price: 19.99,
-  description: "Example Description",
-};
+import CustomCard from "./CustomCard";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return <CustomCard />;
 };
 
 export default Explore;
